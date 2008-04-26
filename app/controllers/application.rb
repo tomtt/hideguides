25d436ce14352e7865c312c5d21dff99189337b6
Tom ten Thij <git@tomtenthij.nl>
2008-04-26 15:43:56 +0100

    generated from rails command

---
+++ app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '000894c5f07c5903f43a23a6b19c483f'
+end
