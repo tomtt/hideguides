5a9e15555bed89dad668a15bbf934a4ce9d292bd
Tom ten Thij <git@tomtenthij.nl>
2008-04-28 00:21:39 +0100

    Passing login_story

--- stories/helper.rb
+++ stories/helper.rb
@@ -1,3 +1,13 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'spec/rails/story_adapter'
+require 'ruby-debug'
+
+dir = File.dirname(__FILE__)
+Dir[File.expand_path("#{dir}/steps/*.rb")].uniq.each do |file|
+  require file
+end
+
+def run_local_story(filename, options={})
+  run File.join(File.dirname(__FILE__), 'texts', filename), options
+end
---
+++ stories/scripts/login_story.rb
@@ -0,0 +1,5 @@
+require File.join(File.dirname(__FILE__), '..', 'helper')
+
+with_steps_for(:login) do
+  run_local_story "login_story", :type => RailsStory
+end
